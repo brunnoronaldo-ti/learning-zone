81dd2a08760f0cedeef7a7a700552cdd5b706ea8
brunno ronaldo <brunnoronaldo18@gmail.com>
2025-12-20 18:55:37 -0300

    Add comments explaining password generation process
    Added explanation comments for password generation logic.

--- estudos-python/gerador-de-senha.py
+++ estudos-python/gerador-de-senha.py
@@ -1,3 +1,12 @@
 import string
 import random
 print('Password: ', ''.join(random.choice(string.ascii_letters + string.digits) for _ in range (8)))
+
+#EXPLICAÇÃO:
+#"string.ascii_letters + string.digits" entrega todas as letras e números, ele basicamente diz:
+#abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
+#---------------------------------------------------------------------------------------------
+#"random.choice" diz: "vou pegar algum caractere aleatório disso tudo"
+#"for _ in range (8)" isso limita o número de caracteres até '8'
+#---------------------------------------------------------------------------------------------
+# " ''.join " e´a cola, sem ela vocÊ teria uma lista feia, com ela você tem uma senha arrumada
