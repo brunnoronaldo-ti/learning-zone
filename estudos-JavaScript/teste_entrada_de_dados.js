c6057d454a55452dd5f3c78774aa35e848d956b3
brunno ronaldo <brunnoronaldo18@gmail.com>
2026-01-15 00:20:23 -0300

    Add age input and validation in teste_entrada_de_dados.js

---
+++ estudos-JavaScript/teste_entrada_de_dados.js
@@ -0,0 +1,15 @@
+console.log("insira a idade:")
+
+//inserir idade
+// Usando prompt para receber a idade do usuário
+// Convertendo a entrada para número usando Number()
+// Armazenando o valor na variável idade
+
+let idade = Number(prompt("Qual sua idade?"))
+
+// 'if' e 'else' para verificar se a idade é maior ou menor de 18
+if (idade >= 18) {
+    console.log("maior de idade")
+} else {
+    console.log("menor de idade")
+}
