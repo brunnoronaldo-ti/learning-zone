7b9f54dd0dabd7901810d7b21fab591d4d225547
brunno ronaldo <brunnoronaldo18@gmail.com>
2026-02-08 02:10:36 -0300

    Create fist-code.cs

---
+++ estudos-csharp/fist-code.cs
@@ -0,0 +1,16 @@
+//input
+
+using System;; // usando o namespace System
+// comentário de linha
+/* comentário   de bloco */
+
+namespace TesteNamespace // declaração do namespace
+{
+    class TesteClasse // declaração da classe
+    {
+        static void Main(string[] args) // ponto de entrada do programa
+        {
+            Console.WriteLine("Olá, Mundo!"); // imprime uma mensagem
+        }
+    }
+}
