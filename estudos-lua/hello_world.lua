a085a77b28480268c854ba98a27f03bcee1b2061
brunno ronaldo <brunnoronaldo18@gmail.com>
2026-03-22 19:32:57 -0300

    Add Lua examples for basic data types and tables

---
+++ estudos-lua/hello_world.lua
@@ -0,0 +1,6 @@
+local nome = "lua test"     -- String
+local versao = 2.0          -- Number (não separa int de float)
+local status = true         -- Boolean
+local nada = nil            -- O "None" ou "null"
+
+-- Unlike Python (dynamic typing) and JS (let/const), in Lua we use local. Always use local, or the variable becomes global and goes out of scope.
---
+++ estudos-lua/tables_and_dictionaries.lua
@@ -0,0 +1,12 @@
+-- Como Array (Index starts at 1!)
+local fruit = {"apple", "banana", "orange"}
+print(fruit[1]) -- exit: apple
+
+-- Como Dicionário
+local player = {
+    nome = "Dev brunno",
+    level = 10
+}
+print(player.nome) -- Or player["nome"]
+
+-- In Lua, anything that isn't a basic type is a Table.
