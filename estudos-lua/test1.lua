9213cd5fac1a86c5ceae8f340c5ed54c894d927e
brunno ronaldo <brunnoronaldo18@gmail.com>
2026-03-22 19:51:35 -0300

    renamed:    estudos-lua/tables_and_dictionaries.lua -> estudos-lua/test2.lua

--- estudos-lua/test1.lua
+++ estudos-lua/test1.lua
@@ -3,4 +3,6 @@ local versao = 2.0          -- Number (não separa int de float)
 local status = true         -- Boolean
 local nada = nil            -- O "None" ou "null"
 
--- Unlike Python (dynamic typing) and JS (let/const), in Lua we use local. Always use local, or the variable becomes global and goes out of scope.
+-- Unlike Python (dynamic typing) and JS (let/const), in Lua we use local.
+-- Always use local, or the variable becomes global and goes out of scope.
+
