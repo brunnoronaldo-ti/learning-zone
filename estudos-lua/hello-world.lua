a183a1412c5c219688da1bff2d485799465778ca
brunno ronaldo <brunnoronaldo18@gmail.com>
2026-03-23 23:27:54 -0300

    Add hello-world.lua with introductory Lua examples and comments

---
+++ estudos-lua/hello-world.lua
@@ -0,0 +1,13 @@
+print("hello world")
+print("hello, Lua!")
+print("Lua is a lightweight and powerful scripting language.")
+print("Lua is great for games, automation, and much more!")
+print("Welcome to the world of programming with Lua!")
+
+print("Lua is fun to learn and use!")
+print("Happy coding with Lua!")
+
+print()
+print("for comments, use -- for single line and --[[ ]]-- for multi-line.")
+-- example of a single line comments
+print("for more Lua tutorials, visit: https://www.lua.org/pil/")
